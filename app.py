b7aa12f387b3cf51aaa5f983d4197df0942d30d8
Baligh Mnassri <baligh.mnassri@gmail.com>
2020-06-01 17:22:24 +0200

    Commit all app components

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+
+from futils.covidfunctions import charts, overall_departments_data_as_json, department_label
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def graphs():
+    """Country page of the app"""
+    graphJSON, ids, counters = charts(department='ALL')
+    overall_departments_data, overall_departments_quantiles = overall_departments_data_as_json()
+    
+    return render_template(
+        "graphs.html", 
+        graphJSON = graphJSON, 
+        ids = ids, 
+        counters = counters,
+        label = "France",
+        department = '',
+        overall_departments_data = overall_departments_data,
+        overall_departments_quantiles = overall_departments_quantiles,
+    )
+
+
+@app.route('/departement/<string:department>')
+def view_department(department):
+    """Department page of the app"""
+    graphJSON, ids, counters = charts(department)
+    overall_departments_data, overall_departments_quantiles = overall_departments_data_as_json()
+
+    label = department_label(department)
+    if label == "":
+        label = "France"
+        department = ""
+
+    return render_template(
+        "graphs.html", 
+        graphJSON = graphJSON, 
+        ids = ids, 
+        counters = counters,
+        label = label,
+        department = department,
+        overall_departments_data = overall_departments_data,
+        overall_departments_quantiles = overall_departments_quantiles,
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
